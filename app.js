f60181eb2c737ab77445e2b0b022a4473cb39613
Alvaro Cabanillas <106959153+alcabanillas@users.noreply.github.com>
2023-03-05 17:59:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
-//app.js
-const express = require("express");
-const app = express();
+// contacts data is hardwired into memory
+var express = require("express");
+var app = express();
+const fetch = require("node-fetch");
+app.use(express.json());
+// store contacts in an arrays
 var contacts = [
   {
     name: "peter parker",
     age: 21,
     email: "peter@mit.edu",
+    courses: [
+      { number: "1.00", name: "engr comp" },
+      { number: "3.00", name: "intro bio" },
+    ],
   },
   {
     name: "bruce wayne",
     age: 32,
     email: "bruce@mit.edu",
+    courses: [
+      { number: "2.00", name: "intro ME" },
+      { number: "3.00", name: "intro MS" },
+    ],
   },
   {
     name: "diana prince",
     age: 25,
     email: "diana@mit.edu",
+    courses: [
+      { number: "2.00", name: "intro arch" },
+      { number: "1.00", name: "intro chem" },
+    ],
   },
 ];
+
 app.get("/", function (req, res) {
-  res.send("<h1> Routes: try POST to /contact or GET /contacts </h1>");
+  res.send("<h1> Goodbye Routes: try POST to /contact and GET /contacts </h1>");
 });
 // list all contacts
 app.get("/contacts", function (req, res) {
@@ -31,4 +47,5 @@ app.post("/contact", (req, res) => {
   res.json(req.body);
 });
 
-module.exports = app;
+app.listen(3000);
+console.log("Running on port 3000");
